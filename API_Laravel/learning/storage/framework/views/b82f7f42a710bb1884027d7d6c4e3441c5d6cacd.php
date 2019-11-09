9663a089d45a973c9c604b5cc97a8e03c0420387
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2019-11-09 12:37:45 +0700

    Added files that affected by gitignore

---
+++ API_Laravel/learning/storage/framework/views/b82f7f42a710bb1884027d7d6c4e3441c5d6cacd.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\SEMESTER 7 TEMP\Keisengan\API_Laravel\learning\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
